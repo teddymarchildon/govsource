cc0e1505c7d6d6ca71bcbaedf31685c54decc2c0
teddymarchildon <ted.marchildon@gmail.com>
2025-06-11 07:54:58 -0400

    Add analytics back

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({
             </main>
           </div>
         </AuthProvider>
-        {/* <Analytics /> */}
+        <Analytics />
         <SpeedInsights />
       </body>
     </html>
