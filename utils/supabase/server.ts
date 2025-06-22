89b51b1a36c0d4b921b6020b3b095cdad3c920f6
teddymarchildon <ted.marchildon@gmail.com>
2025-06-22 15:57:52 -0400

    Use supabase ssr auth

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,9 +1,20 @@
-import { createClient } from '@supabase/supabase-js';
+import { createServerClient } from '@supabase/ssr'
+import { cookies } from 'next/headers'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY || '';
-
-// Create a single supabase client for server-side rendering
-export const supabaseServer = createClient(supabaseUrl, supabaseKey, {
-  auth: { persistSession: false }
-});
+const cookieStore = await cookies()
+export const supabaseServer = createServerClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!,
+  {
+    cookies: {
+      getAll() {
+        return cookieStore.getAll()
+      },
+      setAll(cookiesToSet) {
+        cookiesToSet.forEach(({ name, value, options }) =>
+          cookieStore.set(name, value, options)
+        )
+      },
+    },
+  }
+)
