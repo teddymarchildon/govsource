02df636c5351a8b2192882aa8bfca800e2aa1b94
teddymarchildon <ted.marchildon@gmail.com>
2025-06-28 16:22:33 -0400

    Remove log

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -76,7 +76,6 @@ export function AuthProvider({ children }: { children: ReactNode }) {
   }, []);
 
   const signInWithMagicLink = async (email: string, redirectUrl?: string) => {
-    console.log('signInWithMagicLink', email, redirectUrl);
     const { error } = await supabase.auth.signInWithOtp({
       email,
       options: {
