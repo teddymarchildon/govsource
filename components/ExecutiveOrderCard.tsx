d8de162562a836f96a7cbe2dbcad3ee8a4139daf
teddymarchildon <ted.marchildon@gmail.com>
2025-06-29 17:19:38 -0400

    Simplify executive order card

--- components/ExecutiveOrderCard.tsx
+++ components/ExecutiveOrderCard.tsx
@@ -21,36 +21,22 @@ export default function ExecutiveOrderCard({ order }: ExecutiveOrderCardProps) {
   return (
     <Card className="h-full flex flex-col hover:shadow-md transition-shadow duration-200">
       <CardHeader className="p-4 pb-2">
-        <CardTitle className="text-base font-semibold text-gray-700">
-          {order.remote_document_number}
-        </CardTitle>
-      </CardHeader>
-
-      <CardContent className="flex-grow px-4 pb-2">
         <Link
           href={`/executive-orders/${order.id}`}
           className="block hover:text-blue-600 transition-colors"
         >
-          <p className="text-sm font-medium text-gray-900 line-clamp-3">
+          <CardTitle className="text-base font-semibold text-gray-900 line-clamp-2">
             {order.title}
-          </p>
+          </CardTitle>
         </Link>
+      </CardHeader>
+
+      <CardContent className="flex-grow px-4 pb-2">
+        {/* No title here, only abstract or other info if needed */}
       </CardContent>
 
       <CardFooter className="p-4 pt-2">
         <div className="flex flex-col space-y-2 text-xs w-full mt-auto">
-          {order.agency && (
-            <div className="text-gray-700">
-              <span className="font-medium">Agency:</span>{' '}
-              <Link
-                href={`/agencies/${order.agency.id}`}
-                className="text-blue-600 hover:underline"
-              >
-                {order.agency.name}
-              </Link>
-            </div>
-          )}
-
           {order.president && (
             <div className="text-gray-700">
               <span className="font-medium">President:</span>{' '}
