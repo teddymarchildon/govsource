9584e37d7bc084c6f8a26328e4ed24ccb8c8f7af
teddymarchildon <ted.marchildon@gmail.com>
2025-06-30 12:04:53 -0400

    Make font not bold on sign out

--- components/Header.tsx
+++ components/Header.tsx
@@ -154,7 +154,7 @@ export default function Header() {
                     </Link>
                     <Button
                       variant="ghost"
-                      className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                      className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 font-normal"
                       onClick={handleSignOut}
                     >
                       Sign Out
@@ -250,7 +250,7 @@ export default function Header() {
                     </Link>
                     <Button
                       variant="ghost"
-                      className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                      className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 font-normal"
                       onClick={handleSignOut}
                     >
                       Sign Out
